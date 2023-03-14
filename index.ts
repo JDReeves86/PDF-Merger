386f76ba5046f285700fe2afde998f0b64e12b20
JDReeves86 <Reeves.Jacobd@gmail.com>
2023-03-14 08:55:55 -0700

    converted to async functions

--- index.ts
+++ index.ts
@@ -5,13 +5,10 @@ import util = require("util");
 require("dotenv").config();
 import inquirer from "inquirer";
 import PDFMerger = require("pdf-merger-js");
-import Choice = require("inquirer/lib/objects/choice");
 
 let pdfDir: String = process.env.PDF_HOME;
 const readdir = util.promisify(fs.readdir);
 let merger: PDFMerger = new PDFMerger();
-
-let fileMerger: Object[] = [];
 
 const directoryPrompts: [Object] = [
   {
@@ -22,19 +19,21 @@ const directoryPrompts: [Object] = [
   },
 ];
 
-const directoryQuestions: Function = () => {
-  inquirer
-    .prompt(directoryPrompts)
-    .then((response: { sourceDir: string }) => {
-      let fileList: Object[] = makePDFList(response.sourceDir);
-      return fileList;
-    })
-    .then((data) => {
-      return fileQuestions(data);
-    })
+const directoryQuestions: Function = async () => {
+  try {
+    interface Response {
+      sourceDir: string
+    }
+    const response: {sourceDir: string} = await inquirer.prompt(directoryPrompts)
+    const sorted: Object[] = await makePDFList(response.sourceDir)
+    return fileQuestions(sorted)
+  }
+  catch (error) {
+    console.log(error)
+  }
 };
 
-const fileQuestions: Function = (input: Object[]) => {
+const fileQuestions: Function = async (input: Object[]) => {
   const filePrompts: Object[] = [
     {
       type: "list",
@@ -50,27 +49,27 @@ const fileQuestions: Function = (input: Object[]) => {
       choices: ["Whole document", "Single page", "Page range"],
     },
     {
-      type: 'input',
-      name: 'singlePage',
-      message: 'Which page do you wish to add?',
+      type: "input",
+      name: "singlePage",
+      message: "Which page do you wish to add?",
       when(response: { pages: string }) {
-        return response.pages === 'Single page'
+        return response.pages === "Single page";
       },
     },
     {
-      type: 'input',
-      name: 'startPageRange',
-      message: 'Which page do you wish to start the add?',
+      type: "input",
+      name: "startPageRange",
+      message: "Which page do you wish to start the add?",
       when(response: { pages: string }) {
-        return response.pages === 'Page range'
+        return response.pages === "Page range";
       },
     },
     {
-      type: 'input',
-      name: 'endPageRange',
-      message: 'Which page do you wish to stop the add?',
+      type: "input",
+      name: "endPageRange",
+      message: "Which page do you wish to stop the add?",
       when(response: { pages: string }) {
-        return response.pages === 'Page range'
+        return response.pages === "Page range";
       },
     },
     {
@@ -80,39 +79,41 @@ const fileQuestions: Function = (input: Object[]) => {
       choices: ["Yes", "No"],
     },
     {
-      type: 'input',
-      name: 'output',
-      message: 'What do you want the name of the merged file to be?',
-      when (response: {addMore: string }) {
-        return response.addMore === 'No'
-      }
+      type: "input",
+      name: "output",
+      message: "What do you want the name of the merged file to be?",
+      when(response: { addMore: string }) {
+        return response.addMore === "No";
+      },
     },
   ];
-  return inquirer.prompt(filePrompts).then(async (response) => {
+  try {
+    const response = await inquirer.prompt(filePrompts);
     switch (response.pages) {
-      case 'Single page':
-        await merger.add(`${pdfDir}/${response.fileSelect}`, response.singlePage)
-        break
-      case 'Page range':
-        await merger.add(`${pdfDir}/${response.fileSelect}`, `${response.startPageRange} to ${response.endPageRange}`)
-        break
+      case "Single page":
+        await merger.add(
+          `${pdfDir}/${response.fileSelect}`,
+          response.singlePage
+        );
+        break;
+      case "Page range":
+        await merger.add(
+          `${pdfDir}/${response.fileSelect}`,
+          `${response.startPageRange} to ${response.endPageRange}`
+        );
+        break;
       default:
-        await merger.add(`${pdfDir}/${response.fileSelect}`)
-        break
+        await merger.add(`${pdfDir}/${response.fileSelect}`);
+        break;
     }
-    // if (response.pages === 'Single page') {
-    //   await merger.add(`${pdfDir}/${response.fileSelect}`, response.singlePage)
-    // }
-    // if (response.pages === 'Page range') {
-    //   await merger.add(`${pdfDir}/${response.fileSelect}`, `${response.startPageRange} to ${response.endPageRange}`)
-    // }
     if (response.addMore == "No") {
-      // await merger.add(`${pdfDir}/${response.fileSelect}`)
-      await merger.save(`${pdfDir}/${response.output}.pdf`)
+      await merger.save(`${pdfDir}/${response.output}.pdf`);
     } else {
-      return fileQuestions(input)
+      return fileQuestions(input);
     }
-  });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const init: Function = () => {
@@ -123,17 +124,6 @@ const makePDFList: Function = async (directory: fs.PathLike) => {
   try {
     const fileArr: Object = await readdir(directory);
     return fileArr;
-  } catch (err) {
-    console.error(err);
-  }
-};
-
-const mergePDFs: Function = async (input: [Object]) => {
-  try {
-    for (const file of input) {
-      await merger.add(`${pdfDir}/${file}`);
-    }
-    await merger.save(`${pdfDir}/merged.pdf`);
   } catch (err) {
     console.error(err);
   }
